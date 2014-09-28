51f9fe0b9b3f32069ce2a1c4a14a87409013f9ac
jazzboysc <jazzboysc@gmail.com>
2014-09-28 19:56:01 -0400

    ....

--- VPL/VPLApp.h
+++ VPL/VPLApp.h
@@ -104,7 +104,7 @@ private:
     Texture2DArrayPtr mVPLShadowMapDepthTextureArray;
 
     // VPL stuff.
-    enum { VPL_SAMPLE_COUNT = 64 };
+    enum { VPL_SAMPLE_COUNT = 128 };
     Texture1DPtr mVPLSamplePattern;
     Texture1DPtr mVPLSampleTest;
     StructuredBufferPtr mVPLBuffer;
