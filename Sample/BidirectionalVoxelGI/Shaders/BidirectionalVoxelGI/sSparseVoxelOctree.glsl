db20d69b6c4963970c8d21c7591a23b7ee671885
jazzboysc@gmail.com <S515380c>
2015-02-26 14:26:51 -0500

    ....

--- Sample/BidirectionalVoxelGI/Shaders/BidirectionalVoxelGI/sSparseVoxelOctree.glsl
+++ Sample/BidirectionalVoxelGI/Shaders/BidirectionalVoxelGI/sSparseVoxelOctree.glsl
@@ -2,6 +2,7 @@
 #define SVO_MAX_LEVEL 7
 #define SVO_MAX_LEVEL_DIM 128
 #define SVO_RAY_T_EPSILON 0.0001
+#define SVO_RAY_ENDPOINT_BIAS 2.5
 
 #define SVO_NODE_LEAF_MASK         0x80000000
 #define SVO_NODE_FLAG_MASK         0x40000000
@@ -248,5 +249,107 @@ ivec3 WorldToGridPosition(vec3 worldPosition)
     ivec3 res = ivec3(offsets);
 
     return res;
+}
+//----------------------------------------------------------------------------
+uint SVOIntersectionTest(vec3 rayStartPoint, vec3 rayEndPoint)
+{
+    float minT, maxT, sceneMinT, sceneMaxT;
+
+    // Transform world space ray to SVO space.
+    vec3 sceneBBMin = voxelFragmentBuffer.SceneBBMin.xyz;
+    vec3 inv2SceneBBExtension = voxelFragmentBuffer.Inv2SceneBBExtension.xyz;
+    vec3 rayStartPosSVO = (rayStartPoint - sceneBBMin)*inv2SceneBBExtension*
+        SVO_MAX_LEVEL_DIM;
+    vec3 rayEndPosSVO = (rayEndPoint - sceneBBMin)*inv2SceneBBExtension*
+        SVO_MAX_LEVEL_DIM;
+
+    // Initialize sceneMinT, sceneMaxT and ray direction.
+    vec3 rayDirSVO = rayEndPosSVO - rayStartPosSVO;
+    sceneMinT = 0.0;
+    sceneMaxT = length(rayDirSVO);
+    rayDirSVO = rayDirSVO / sceneMaxT;
+    rayStartPosSVO = rayStartPosSVO + rayDirSVO*SVO_RAY_ENDPOINT_BIAS;
+    rayEndPosSVO = rayEndPosSVO - rayDirSVO*SVO_RAY_ENDPOINT_BIAS;
+    vec3 rayInvDirSVO = 1.0 / rayDirSVO;
+
+    uint hit = 0;
+    minT = maxT = sceneMinT;
+    SVONode curNode, root;
+    vec3 rayEntryPos;
+
+    // Initialize root node.
+    root.info = SVO_NODE_FLAG_MASK;
+    root.nodeBox.Min = Ivec3ToUint(ivec3(0, 0, 0));
+    root.nodeBox.Max = Ivec3ToUint(ivec3(SVO_MAX_LEVEL_DIM,
+        SVO_MAX_LEVEL_DIM, SVO_MAX_LEVEL_DIM));
+
+    uint level, childIndex, nextNodeIndex;
+    while( maxT < sceneMaxT )
+    {
+        // Restart traversal from root.
+        curNode = root;
+        minT = maxT;
+        maxT = sceneMaxT;
+        rayEntryPos = rayStartPosSVO + rayDirSVO*minT;
+
+        // Descend until we meet a leaf node.
+        bool isLeaf = IsSVOLeafNode(curNode);
+        while( !isLeaf )
+        {
+            // Figure out which child node we are in.
+            childIndex = GetSVOChildNodeIndex(rayEntryPos, curNode.nodeBox);
+
+            // Locate child node for current tree level.
+            uint childrenID = (curNode.info & SVO_NODE_CHILDREN_ID_MASK);
+            nextNodeIndex = childrenID*SVO_NODE_TILE_SIZE + childIndex;
+
+            // Update next node to visit.
+            curNode = svoNodeBuffer.data[nextNodeIndex];
+            isLeaf = IsSVOLeafNode(curNode);
+        }
+
+        // Deal with the leaf node.
+        if( IsSVONodeFlaged(curNode) )
+        {
+            hit = 1;
+            break;
+        }
+
+        // Find maxT for the current node.
+
+        vec3 nodeBoxMin = vec3(UintToIvec3(curNode.nodeBox.Min));
+        vec3 nodeBoxMax = vec3(UintToIvec3(curNode.nodeBox.Max));
+        vec3 minMinusOrigin = nodeBoxMin - rayStartPosSVO;
+        vec3 maxMinusOrigin = nodeBoxMax - rayStartPosSVO;
+        float t0 = minT, t1 = maxT;
+
+        for( int k = 0; k < 3; ++k )
+        {
+            float tNear = minMinusOrigin[k] * rayInvDirSVO[k];
+            float tFar = maxMinusOrigin[k] * rayInvDirSVO[k];
+
+            if( tNear > tFar )
+            {
+                // Swap.
+                float temp = tNear;
+                tNear = tFar;
+                tFar = temp;
+            }
+
+            t0 = tNear > t0 ? tNear : t0;
+            t1 = tFar  < t1 ? tFar : t1;
+
+            if( t0 > t1 )
+            {
+                return 0;
+            }
+        }
+
+        // Update maxT.
+        maxT = t1 + SVO_RAY_T_EPSILON;
+
+    }
+
+    return hit;
 }
 //----------------------------------------------------------------------------
